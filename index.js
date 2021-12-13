f3fd382f9d774306c4f5823cd06e3d37c8a02387
Guest <guest-dkw5d1@h28091.sgb.skir.kr>
2021-12-13 22:21:57 +0900

    database

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', (req, res) => {
+	res.redirect('/contacts');
+});
 
 app.get('/hello', (req, res) => {
 	res.render('hello', {name:req.query.nameQuery});
