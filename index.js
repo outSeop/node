271703db97d15c9ccce600942fa794f23896720a
outSeop <sis000419@gmail.com>
2021-12-22 23:07:22 +0900

    db

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var app = express();
 
@@ -7,16 +8,40 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+mongoose.connect("mongodb+srv://inssong:seop@cluster0.5wcrw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+var db = mongoose.connection;
+db.once('open', () => {
+	console.log('DB connected');
+});
+db.on('error', (err) => {
+	console.log('DB ERROR: ', err);
+});
+var contactSchema = mongoose.Schema({
+	name:{type:String, require:true, unique:true},
+	email:{type:String},
+	phone:{tpye:String}
+});
+var Contact = mongoose.model('contact', contactSchema);
+
+
 app.get('/', (req, res) => {
 	res.redirect('/contacts');
 });
-
-app.get('/hello', (req, res) => {
-	res.render('hello', {name:req.query.nameQuery});
+app.get('/contacts', (req, res) => {
+	Contact.find({}, (err, contacts) => {
+		if (err) return res.json(err);
+		res.render('contacts/index', {contacts:contacts});
+	});
 });
 
-app.get('/hello/:nameParam', (req, res) => {
-	res.render('hello', {name:req.params.nameParam});
+app.get('/contacts/new', (req, res) => {
+	res.render('contacts.new');
+});
+
+app.post('/contacts', (req, res) => {
+	Contact.create(req.body, (err, contact) => {
+		res.redirect('/contacts');
+	});
 });
 
 var port = 3000;
